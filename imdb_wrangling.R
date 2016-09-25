bf2ed5da236792dd79d3ecef1b03cb470d590481
mkarim <muhsinkarim@outlook.com>
2016-09-25 13:05:54 +1000

    Some change I can't remember

--- imdb_wrangling.R
+++ imdb_wrangling.R
@@ -13,6 +13,8 @@
 
     #devtools::install_github("hrbrmstr/omdbapi")
     library(omdbapi)
+    library(dplyr)
+    library(shiny)
 
 
 #---------------------------------------------------------------------------------------------------------------------------
@@ -20,6 +22,7 @@
 
     tvSeries <- "Breaking Bad"
 #    tvSeries <- "Community"
+#    tvSeries <- "Game of Thrones"
 
     
 #---------------------------------------------------------------------------------------------------------------------------
@@ -64,8 +67,126 @@
         } 
     }
 
+    
+### Select columns
+    
+    df <- select(df, Title, Year, Rated, Released, Season, Episode, Runtime, Genre, Director, Writer, Actors, Plot, 
+                 Language, Country, imdbRating, imdbVotes, imdbID, seriesID)
 
+    
 #---------------------------------------------------------------------------------------------------------------------------
 # Module: Get writers data
     
-                            
+
+### Create data frame with episode writer on each line
+    
+    ## Create writers data frame
+    dfSubset <- select(df, imdbID, Writer)
+    
+    ## Create empty data frame
+    dfWriters <- data.frame(matrix(NA, nrow = 0, ncol = 3))
+    colnames(dfWriters) <- c(colnames(dfSubset), "Is writer")
+    
+    
+    ## Iterate through each row
+    for (i in 1:nrow(dfSubset)) {
+        
+        ## Get writers
+        theWriters <- dfSubset$Writer[i]
+        
+        ## Split writers
+        theWriters <- unlist(strsplit(theWriters, ","))
+        
+        ## Place each writer on a seperate line
+        for (j in theWriters) {
+            
+            ## Check if writer of episode
+            # If brackets present, likely not a writer
+            if (grepl("\\)" , j)) {
+                isWriters <- "No" # Not writer of episode
+            } else {
+                isWriters <- "Yes" # Writer of episode
+            }
+                        
+            ## Get the Id, Writer and Is writer
+            dfWritersAdd <- t(as.data.frame(c(df$imdbID[i], j, isWriters)))
+            rownames(dfWritersAdd) <- NULL        
+            colnames(dfWritersAdd) <- c(colnames(dfSubset), "isWriter")
+
+            ## Bind
+            dfWriters <- rbind.data.frame(dfWriters, dfWritersAdd)
+        }
+    }
+   
+     
+### Create writers data frame with writer on each line
+    
+    ## Remove Writer column from df
+    dfTemp <- select(df, -Writer)
+    
+    ## Merge data frames
+    dfWriters <- left_join(dfWriters, dfTemp, by = "imdbID")
+    
+    ## Remove leading and trailing whitespace
+    dfWriters$Writer <- gsub("^\\s+|\\s+$", "", dfWriters$Writer)
+        
+    ## Rearange columns
+    dfWriters <- dfWriters %>%
+        select(
+            Title:Director,
+            Writer,
+            isWriter,
+            Actors:imdbVotes,
+            imdbID,
+            seriesID
+        )
+    
+    
+#---------------------------------------------------------------------------------------------------------------------------
+# Module: Plots
+
+### Try latest Github version of ggraptR    
+    
+    #devtools::install_github('cargomoose/raptR', force = TRUE) # install
+    library("ggraptR") 
+    
+    
+### Number of episodes per writer
+    
+    ## Group by writer
+    dfGroup <-
+        dfWriters %>%
+        filter(isWriter == "Yes") %>%
+        group_by(Writer) %>%
+        summarise(Count = n())
+
+    ## Order by most frequent writer
+    dfGroup <- dfGroup[order(dfGroup$Count, decreasing = T), ]
+    
+    
+    ggraptR()
+    
+    
+    dfGroup$Writer <- reorder(dfGroup$Writer, dfGroup$Count)
+    
+    ggplot(dfGroup, 
+           aes(y = Count, x = Writer)) + 
+        geom_bar(stat = "identity", position = "identity", alpha = 0.5, fill = "#06602B") + 
+        coord_flip() + 
+        theme(text = element_text(color = "black", size = 15, hjust = 0.5, vjust = 0.5)) + 
+        ggtitle("Breaking Bad episodes written per writer") +
+        xlab("Awesome BB writer") + ylab("Number of episodes")
+    
+    
+    
+    
+    ggplot(dfGroup, 
+           aes(y = Count, x =  Writer)) + 
+        geom_bar(stat = "identity", 
+                 position = "identity", 
+                 alpha = 0.5) + 
+        coord_flip() + 
+        theme_grey() + 
+        theme(text = element_text(color = "black", size = 15, hjust = 0.5, vjust = 0.5)) + 
+        xlab("Writer") + ylab("Count")
+    
